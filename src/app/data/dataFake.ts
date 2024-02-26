fa7f45858572828bab70315e6d99248a2e4f26d4
Luiz Marcelo <lluismarcello333@gmail.com>
2024-02-26 15:34:01 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    'id': '1',
+    'title': 'Novo Homem de Ferro em 3D',
+    'description': 'Marvel anuncia um novo filme do homem de ferro. Confira!',
+    'photoCover': 'https://rollingstone.uol.com.br/media/uploads/homem_de_ferro_reproducao.jpg',
+  },
+  {
+    'id': '2',
+    'title': 'Nova Série anunciada no Disney Plus',
+    'description': 'Olá',
+    'photoCover': 'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg',
+  },
+]
